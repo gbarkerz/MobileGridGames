16b883437662eeb24797392e209d1cadb8c7a9a2
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-01-17 10:36:41 +0000

    Add link to app's ReadMe at github.

--- MobileGridGames/MobileGridGames/AppShell.xaml.cs
+++ MobileGridGames/MobileGridGames/AppShell.xaml.cs
@@ -1,26 +1,38 @@
-﻿using MobileGridGames.ViewModels;
-using MobileGridGames.Views;
+﻿using MobileGridGames.Views;
 using System;
-using System.Collections.Generic;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace MobileGridGames
 {
-    public partial class AppShell : Xamarin.Forms.Shell
+    public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
         }
 
-        private async void OnMenuItemClicked(object sender, EventArgs e)
+        private async void OnSettingsMenuItemClicked(object sender, EventArgs e)
         {
+            Shell.Current.FlyoutIsPresented = false;
+
             var settingsPage = new SettingsPage();
 
             await Navigation.PushModalAsync(settingsPage);
+        }
 
+        private async void OnHelpMenuItemClicked(object sender, EventArgs e)
+        {
             Shell.Current.FlyoutIsPresented = false;
+
+            var answer = await DisplayAlert(
+                "Help",
+                "Would you like to visit the developer site which contains details on how to play this game?",
+                "Yes", "No");
+            if (answer)
+            {
+                Browser.OpenAsync("https://github.com/gbarkerz/MobileGridGames/blob/master/README.md");
+            }
         }
     }
 }
