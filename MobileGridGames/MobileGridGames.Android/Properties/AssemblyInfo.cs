bd3242156bc075ad23ed6de754a9ec996c299eb3
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-01-24 20:46:55 +0000

    Add privacy statement.

--- MobileGridGames/MobileGridGames.Android/Properties/AssemblyInfo.cs
+++ MobileGridGames/MobileGridGames.Android/Properties/AssemblyInfo.cs
@@ -28,3 +28,9 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+#if DEBUG
+[assembly: Application(Icon = "@drawable/icon", Debuggable =true)]
+#else
+[assembly: Application(Icon = "@drawable/icon", Debuggable = false)]
+#endif
