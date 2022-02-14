d69afa332d5ce2119c6e939ba6d41324969857ae
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-02-14 10:22:13 +0000

    Refresh Hambuger UI on app theme change

--- MobileGridGames/MobileGridGames/Views/AppSettingsPage.xaml.cs
+++ MobileGridGames/MobileGridGames/Views/AppSettingsPage.xaml.cs
@@ -24,6 +24,13 @@ namespace MobileGridGames
         private async void CloseButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
+
+            // Create a new AppShell here to force the new theme colours to be shown
+            // on the hamburger button.
+
+            // Future: This seems a little heavy-handed just to get the colours to refresh.
+            // Investigate whether there's a simpler way to achieve the results.
+            App.Current.MainPage = new AppShell();
         }
     }
 }
