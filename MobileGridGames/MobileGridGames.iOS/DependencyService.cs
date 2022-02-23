69084d49648e29aeb65004e44f9bf57c0efb00ba
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-02-23 17:09:21 +0000

    Add untested iOS project to the solution.

---
+++ MobileGridGames/MobileGridGames.iOS/DependencyService.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using MobileGridGames.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MobileGridGames.iOS.MobileGridGamesPlatformAction))]
+namespace MobileGridGames.iOS
+{
+    public class MobileGridGamesPlatformAction : IMobileGridGamesPlatformAction
+    {
+        public void ScreenReaderAnnouncement(string notification)
+        {
+            // Future: Make VoiceOver announcement.
+        }
+
+        public void SetAppTheme(App.Theme mode)
+        {
+            // Future: Set current theme.
+        }
+    }
+}
---
+++ MobileGridGames/MobileGridGames.iOS/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace MobileGridGames.iOS
+{
+    // Barker: The build warning below occurred when first building the project. So
+    // I followed the advice and (manually) set the AutoGenerateBindingRedirects to
+    // true in the iOS project file.
+    //    Warning Found conflicts between different versions of the same dependent assembly.
+    //    Please set the "AutoGenerateBindingRedirects" property to true in the project file.
+
+    // Barker: I also get the warning below that others have hit. I'd add parentheses
+    // to avoid the warning, but I don't know where they're meant to go. So let's hope 
+    // things work anyway.
+    //     The condition "'$(IsMacEnabled)' == 'true' And '$(IsAppExtension)' == 'true' Or
+    //     '$(_PlatformName)' == 'macOS'" may have been evaluated incorrectly
+
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+
+            // Barker: VS created the code with the following line which leads to build warnings.
+            //UIApplication.Main(args, null, "AppDelegate");
+
+            // Barker: So replace that line with the following.
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
+    }
+}
