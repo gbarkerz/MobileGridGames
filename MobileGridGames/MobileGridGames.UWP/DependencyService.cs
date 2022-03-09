25789661f1000f2a82cf4acb3b3dc0252676d397
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-03-09 07:55:41 +0000

    Placeholder code for a UWP version of the app.

---
+++ MobileGridGames/MobileGridGames.UWP/DependencyService.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using MobileGridGames.Services;
+
+using static MobileGridGames.App;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MobileGridGames.UWP.MobileGridGamesPlatformAction))]
+namespace MobileGridGames.UWP
+{
+    public class MobileGridGamesPlatformAction : IMobileGridGamesPlatformAction
+    {
+        public void ScreenReaderAnnouncement(string notification)
+        {
+            // Future: Add this action.
+        }
+    }
+}
