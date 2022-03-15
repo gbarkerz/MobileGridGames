1539401cbe43fa6d995c94f2806f0c1dc7fd42d1
guybarker <gbarkerus@msn.com>
2022-03-15 08:19:56 +0000

    First pass at loading custom pictures for Pairs game i iOS version.

--- MobileGridGames/MobileGridGames/Services/IMobileGridGamesPlatformAction.cs
+++ MobileGridGames/MobileGridGames/Services/IMobileGridGamesPlatformAction.cs
@@ -1,7 +1,13 @@
-﻿namespace MobileGridGames.Services
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MobileGridGames.ViewModels;
+using Xamarin.Forms;
+
+namespace MobileGridGames.Services
 {
     public interface IMobileGridGamesPlatformAction
     {
         void ScreenReaderAnnouncement(string notification);
+        Task<string> GetPairsPictureFolder();
     }
 }
