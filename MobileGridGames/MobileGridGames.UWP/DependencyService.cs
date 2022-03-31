7997b141bdc2aeaffbb78a03a89bc5394d878d32
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-03-31 21:08:34 +0100

    Add a first version of the UWP XAML project for the app.

---
+++ MobileGridGames/MobileGridGames.UWP/DependencyService.cs
@@ -0,0 +1,43 @@
+﻿using MobileGridGames.Services;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Automation.Peers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MobileGridGames.UWP.MobileGridGamesPlatformAction))]
+namespace MobileGridGames.UWP
+{
+    public class MobileGridGamesPlatformAction : IMobileGridGamesPlatformAction
+    {
+        private AutomationPeer screenReaderPeer;
+        private string screenReaderAnnouncementGuid = "F614FC96-70E0-40EB-ACF6-4A5F30E38A45";
+
+        public void SetScreenReaderAnnouncementPeer(AutomationPeer screenReaderPeer)
+        {
+            this.screenReaderPeer = screenReaderPeer;
+        }
+
+        public void ScreenReaderAnnouncement(string notification)
+        {
+            if (screenReaderPeer != null)
+            {
+                Debug.WriteLine("UWP Grid Games - Announce: \"" + notification + "\"");
+
+                // Raise a UIA Notification event.
+                screenReaderPeer.RaiseNotificationEvent(
+                     AutomationNotificationKind.Other,
+                     AutomationNotificationProcessing.All,
+                     notification,
+                     screenReaderAnnouncementGuid);
+            }
+        }
+
+        // GetPairsPictureFolder() does not get called whe running on Windows.
+        public Task<string> GetPairsPictureFolder()
+        {
+            var tcs = new TaskCompletionSource<string>();
+            tcs.SetResult("");
+
+            return tcs.Task;
+        }
+    }
+}
